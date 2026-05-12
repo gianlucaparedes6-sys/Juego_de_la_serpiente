6b3d5853a5117a9d59e3992b62fac1de78561c32
gianlucaparedes6-sys <gianlucaparedes6@gmail.com>
2026-05-11 20:09:31 -0500

    Cuerpo de serpiente

--- serpiente.js
+++ serpiente.js
@@ -3,7 +3,15 @@
     const canvas = document.getElementById("canvasJuego");
     const ctx = canvas.getContext("2d");
     const TAMANIO_CELDA = 25; // Tamaño de cada celda en píxeles
-
+    let serpienteX=0;
+    let serpienteY=0;
+    const serpiente=[];
+    serpiente.push({x: 125, y: 125});
+    serpiente.push({x: 250, y: 50});
+    serpiente.push({x: 200, y: 375});
+    serpiente.push({x: 375, y: 200});
+    serpiente.push({x: 0, y: 200});
+    serpiente.push({x: 375, y: 0});
     
 
     // Primera pintura del juego al cargar la página
@@ -37,11 +45,47 @@
 
   ctx.stroke();
 }
+function dibujarSerpiente(){
+
+  ctx.fillStyle = "lime";
+
+  for(let i = 0; i < serpiente.length; i++){
+
+    let parte = serpiente[i];
+
+    ctx.fillRect(
+      parte.x,
+      parte.y,
+      TAMANIO_CELDA,
+      TAMANIO_CELDA
+    );
+  }
+
+}
 
     function dibujarTodo() {
       limpiarCanvas();
       dibujarTablero();
+      dibujarSerpiente();
     }
+function cambiarDireccion(direccion){
 
+  if(direccion == "derecha"){
+    serpienteX += TAMANIO_CELDA;
+  }
 
+  if(direccion == "izquierda"){
+    serpienteX -= TAMANIO_CELDA;
+  }
+
+  if(direccion == "arriba"){
+    serpienteY -= TAMANIO_CELDA;
+  }
+
+  if(direccion == "abajo"){
+    serpienteY += TAMANIO_CELDA;
+  }
+
+  dibujarTodo();
+}
 
